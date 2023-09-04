eb63efeda7bbdf03cd9c6c0ddb57e3efc4c9c7db
하지훈 <jnh03336@gmail.com>
2023-09-04 16:28:49 +0900

    chore: settings for using sqlite3 db

--- src/main/java/com/finance/SpringConfig.java
+++ src/main/java/com/finance/SpringConfig.java
@@ -14,6 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 
 @Configuration
+@PropertySources({
+        @PropertySource("classpath:.env")
+})
 public class SpringConfig {
     private final CompanyAliasRepository companyAliasRepository;
     private final CompanyRepository companyRepository;
