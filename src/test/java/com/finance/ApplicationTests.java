7266ab349cca4ecf3b9d4c0b529d5d78731cc4b1
하지훈 <jnh03336@gmail.com>
2023-12-21 22:47:11 +0700

    test: add integrate test for companySearchController getCompanyByStockCode

--- src/test/java/com/finance/ApplicationTests.java
+++ src/test/java/com/finance/ApplicationTests.java
@@ -1,13 +1,28 @@
 package com.finance;
 
+import com.finance.controller.CompanySearchController;
+import com.finance.dto.CompanyDTO;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	private CompanySearchController companySearchController;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void getCompanyByStockCode(){
+		String stockCode = "005930";
+		CompanyDTO companyDTO = companySearchController.getCompanyByStockCode(stockCode);
+		assertEquals(companyDTO.name, "삼성전자");
+		assertEquals(companyDTO.stockCode, stockCode);
 	}
 
 }
