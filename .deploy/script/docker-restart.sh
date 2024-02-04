42dc978d12983672ac31ee6ee3082a0190ae2b15
하지훈 <jnh03336@gmail.com>
2024-02-04 20:46:45 +0900

    update(deploy): bind application to 3000 port

--- .deploy/script/docker-restart.sh
+++ .deploy/script/docker-restart.sh
@@ -4,4 +4,4 @@ sudo docker stop api || true
 sudo docker rm api || true
 sudo docker image rm api || true
 sudo docker load -i api.tar
-sudo docker run -d -p 80:8080 -v /home/ec2-user/db:/home/db --name api api
+sudo docker run -d -p 3000:8080 -v /home/ec2-user/db:/home/db --name api api
