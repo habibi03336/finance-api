d70e6729b8a8d6da069de37a6f1e8eb3284d86b4
하지훈 <jnh03336@gmail.com>
2024-02-10 22:29:32 +0900

    deploy: change docker continer config
    - open serving port to 3000 port

--- .deploy/script/docker-restart.sh
+++ .deploy/script/docker-restart.sh
@@ -4,4 +4,4 @@ sudo docker stop finance_api || true
 sudo docker rm finance_api || true
 sudo docker image rm finance_api || true
 sudo docker load -i finance_api.tar
-sudo docker run -d --network api_net -v /home/ec2-user/db:/home/db --name finance_api finance_api
+sudo docker run -d -p 3000:8080 -v /home/ec2-user/db:/home/db --name finance_api finance_api
